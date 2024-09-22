8b4c847f74425c4e6e56bbdfacdc8406ca3cc8d7
tahiana Rakotonirina <tahiana.rakotonirina@skeepers.com>
2024-09-22 22:48:46 +0200

    feat(init): init dev
    feat: add composer json
    
    feat: accept php version 7.2

---
+++ src/Controller/SystemCheckController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace  Tax16\SystemCheckBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SystemCheckController
+{
+    /**
+     * @Route("/system-check", name="system_check")
+     */
+    public function check(): Response
+    {
+        return new Response('System is healthy', 200);
+    }
+}
